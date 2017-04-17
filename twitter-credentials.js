4ae0c3589e50a2e608a62adc2b6136da8bc590b9
sriramsitharaman <sriramsitharaman@gmail.com>
2017-04-17 17:13:12 -0400

    Update twitter-credentials.js

--- twitter-credentials.js
+++ twitter-credentials.js
@@ -4,7 +4,7 @@
  */
 module.exports = {
   consumer_key: 'SyLwuJP6pzy4FevmLMOmcWdpf',
-  consumer_secret: 'XfZkuRRj5yqVmReRkAVvVFm9t6vaPHVeoXEdg85Iuqb8k524p',
+  consumer_secret: 'XfZkuRRj5yqVmReRkAVvVFm9t6vaPHVeoXEdg85Iuqb8k524pU',
   access_token_key: '141817420-ViMO9ic2MuVmjw4u04CACINnCA0MIJEs2uaPbkYX',
   access_token_secret: 'LWNQKJYkHJnrAjNsH7LnrkKWmnf5qZ9akizidiWjbLhOy'
 };
