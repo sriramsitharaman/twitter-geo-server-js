01de761e7337fa5fcde934e49631e0383a827549
sriramsitharaman <sriramsitharaman@gmail.com>
2017-05-02 17:39:17 -0400

    Update twitter-credentials.js

--- twitter-credentials.js
+++ twitter-credentials.js
@@ -3,8 +3,8 @@
  * Get yours at http://apps.twitter.com
  */
 module.exports = {
-  consumer_key: 'SyLwuJP6pzy4FevmLMOmcWdpf',
-  consumer_secret: 'XfZkuRRj5yqVmReRkAVvVFm9t6vaPHVeoXEdg85Iuqb8k524pU',
-  access_token_key: '141817420-ViMO9ic2MuVmjw4u04CACINnCA0MIJEs2uaPbkYX',
-  access_token_secret: 'LWNQKJYkHJnrAjNsH7LnrkKWmnf5qZ9akizidiWjbLhOy'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 };
